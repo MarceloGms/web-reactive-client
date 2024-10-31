3721b1bc4def1ebc39b54b788242ecd49b127ebc
jpedro058 <joao_pedro_12345@hotmail.com>
2024-10-31 21:52:13 +0000

    fix client connection

--- client/src/main/java/com/webreactive/Client.java
+++ client/src/main/java/com/webreactive/Client.java
@@ -6,24 +6,24 @@ import com.webreactive.entity.User;
 import java.io.IOException;
 
 public class Client {
-   public static void main(String[] args) throws IOException {
-      String BASE_URL = "http://localhost:8080";
+	public static void main(String[] args) throws IOException {
+		String BASE_URL = "http://host.docker.internal:8081";
 		String MY_URI = "/user";
 		// String MY_URI = "with delay";
 		WebClient.create(BASE_URL)
-					.get()
-					.uri(MY_URI)
-					.retrieve()
-					.bodyToFlux(User.class)
-					.subscribe(System.out::println);
-
+				.get()
+				.uri(MY_URI)
+				.retrieve()
+				.bodyToFlux(User.class)
+				.subscribe(System.out::println);
 
 		System.out.println("Press Enter to exit");
 		System.in.read();
-   }
+	}
 }
 
 // primeiro req
-// falta aqui alguma cena para proteger a ligaçao, deve se dar retry 3 vezes e dps cagar
-// flux.cena(https://...).map(m -> ""+m.getfile()+ " " + m.getRDHC()+"").subscribe(m -> writeFile())
-
+// falta aqui alguma cena para proteger a ligaçao, deve se dar retry 3 vezes e
+// dps cagar
+// flux.cena(https://...).map(m -> ""+m.getfile()+ " " +
+// m.getRDHC()+"").subscribe(m -> writeFile())
