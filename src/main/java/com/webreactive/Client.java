de29a95a170a406654c01ac76893ccbc3dc34932
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-11-02 11:28:00 +0000

    merge client

--- src/main/java/com/webreactive/Client.java
+++ src/main/java/com/webreactive/Client.java
@@ -6,13 +6,8 @@ import com.webreactive.entity.User;
 import java.io.IOException;
 
 public class Client {
-<<<<<<< HEAD:src/main/java/com/webreactive/Client.java
    public static void main(String[] args) throws IOException {
       String BASE_URL = "http://host.docker.internal:8080";
-=======
-	public static void main(String[] args) throws IOException {
-		String BASE_URL = "http://host.docker.internal:8081";
->>>>>>> a1fbb6cd05cdbd8395afa096d126d336e3f38927:client/src/main/java/com/webreactive/Client.java
 		String MY_URI = "/user";
 		// String MY_URI = "with delay";
 		WebClient.create(BASE_URL)
